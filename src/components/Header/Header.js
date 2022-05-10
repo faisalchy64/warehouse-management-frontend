bdf17e6a64811a1cc2523ef5b2d47c8a9e4760b3
Faisal Karim <faisalchy64@gmail.com>
2022-05-10 18:13:30 +0600

    add a signout functionality on header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,17 @@
+import { signOut } from "firebase/auth";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
+import auth from "../../firebase";
 import ActiveLink from "../ActiveLink/ActiveLink";
 
 function Header() {
+    const [user, loading] = useAuthState(auth);
+
+    const handleSignOut = () => {
+        signOut(auth);
+    };
+
     return (
         <Navbar bg="light" expand="sm" fixed="top" variant="light">
             <Container>
@@ -15,8 +24,16 @@ function Header() {
                 <Navbar.Collapse id="nav-menu">
                     <Nav className="ms-auto">
                         <ActiveLink to="/">Home</ActiveLink>
-                        <ActiveLink to="/login">Login</ActiveLink>
-                        <ActiveLink to="/signup">Signup</ActiveLink>
+                        {user ? (
+                            <button id="signout" onClick={handleSignOut}>
+                                Signout
+                            </button>
+                        ) : (
+                            <>
+                                <ActiveLink to="/login">Login</ActiveLink>
+                                <ActiveLink to="/signup">Signup</ActiveLink>
+                            </>
+                        )}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
