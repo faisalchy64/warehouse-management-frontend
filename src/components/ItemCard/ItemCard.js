b557863cd36c6a07b0a124b7ff2ca6c05d29a8ab
Faisal Karim <faisalchy64@gmail.com>
2023-08-07 14:56:40 +0600

    refactor - code on item_card component

--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -10,22 +10,24 @@ function ItemCard({ item }) {
     };
 
     return (
-        <div className="item border border-3 p-3">
+        <div className="item border">
             <img src={img} alt="" />
-            <h3>{name}</h3>
-            <h6>Description: {description}</h6>
-            <h6>Price: ${price}</h6>
-            <h6>
-                Quantity: {quantity} {name === "Banana" ? "Dozen" : "KG"}
-            </h6>
-            <h6>Supplier: {supplier}</h6>
+            <div className="px-3 py-4">
+                <h3>{name}</h3>
+                <h6>Description: {description}</h6>
+                <h6>Price: ${price}</h6>
+                <h6>
+                    Quantity: {quantity} {name === "Banana" ? "Dozen" : "KG"}
+                </h6>
+                <h6>Supplier: {supplier}</h6>
 
-            <button
-                onClick={handleUpdate}
-                className="update-btn d-block btn btn-danger text-white mt-4 mx-auto"
-            >
-                Update
-            </button>
+                <button
+                    onClick={handleUpdate}
+                    className="update-btn d-block btn btn-danger text-white mt-4 mx-auto"
+                >
+                    Update
+                </button>
+            </div>
         </div>
     );
 }
